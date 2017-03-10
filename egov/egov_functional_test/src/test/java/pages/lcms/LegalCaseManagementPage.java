f6753c6d197935518525867f19bbe628949b923c
bimallenka <bimalendu_lenka@cms.co.in>
2017-03-10 11:38:36 +0530

    [PHOENIX-6395] added legal case feature

--- egov/egov_functional_test/src/test/java/pages/lcms/LegalCaseManagementPage.java
+++ egov/egov_functional_test/src/test/java/pages/lcms/LegalCaseManagementPage.java
@@ -441,7 +441,7 @@ public class LegalCaseManagementPage extends BasePage {
 
     private void enterStandingCouncilDetails(){
 
-        enterText(standingCouncilName , "S.D.Gowd" , webDriver);
+        enterText(standingCouncilName , "S.D.Gowd Advocate & MSC High Court" , webDriver);
         WebElement element = webDriver.findElement(By.className("tt-dataset-0"));
         clickOnButton(element ,webDriver);
 
