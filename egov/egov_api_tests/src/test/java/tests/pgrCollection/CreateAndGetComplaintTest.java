c6796b72fb750fbb1a75e5bfd287d08757c94d9f
karthik45 <karthikpenujuru@gmail.com>
2017-03-16 12:48:25 +0530

    Added pgr collection module

---
+++ egov/egov_api_tests/src/test/java/tests/pgrCollection/CreateAndGetComplaintTest.java
@@ -0,0 +1,15 @@
+package tests.pgrCollection;
+
+
+import org.testng.annotations.Test;
+import tests.BaseAPITest;
+
+import java.io.IOException;
+
+public class CreateAndGetComplaintTest extends BaseAPITest {
+
+    @Test
+    public void createAndGetComplaintInPGR() throws IOException{
+
+    }
+}
