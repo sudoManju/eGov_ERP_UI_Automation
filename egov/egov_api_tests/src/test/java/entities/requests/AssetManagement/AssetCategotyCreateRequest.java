dd25c7696ee7c73ef879617b35f5782f45e07a73
karthik45 <karthikpenujuru@gmail.com>
2017-03-21 11:14:54 +0530

    Added new module assetManagement

---
+++ egov/egov_api_tests/src/test/java/entities/requests/AssetManagement/AssetCategotyCreateRequest.java
@@ -0,0 +1,30 @@
+package entities.requests.AssetManagement;
+
+
+public class AssetCategotyCreateRequest
+{
+    private AssetCategory AssetCategory;
+
+    private RequestInfo RequestInfo;
+
+    public AssetCategory getAssetCategory ()
+    {
+        return AssetCategory;
+    }
+
+    public void setAssetCategory (AssetCategory AssetCategory)
+    {
+        this.AssetCategory = AssetCategory;
+    }
+
+    public RequestInfo getRequestInfo ()
+    {
+        return RequestInfo;
+    }
+
+    public void setRequestInfo (RequestInfo RequestInfo)
+    {
+        this.RequestInfo = RequestInfo;
+    }
+
+}
