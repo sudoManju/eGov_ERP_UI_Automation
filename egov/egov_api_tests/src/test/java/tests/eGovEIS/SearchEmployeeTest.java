4b95eb81e8a48861e081d6f97d668549640ddfce
Manjunath <manjunatha.s@egovernments.org>
2017-03-23 18:47:02 +0530

    [PHOENIX-6513]-api: Search Employee test completed

--- egov/egov_api_tests/src/test/java/tests/eGovEIS/SearchEmployeeTest.java
+++ egov/egov_api_tests/src/test/java/tests/eGovEIS/SearchEmployeeTest.java
@@ -27,13 +27,27 @@ public class SearchEmployeeTest extends BaseAPITest {
         Map jsonData = RequestHelper.asMap(searchEmployeeRequest);
 
         Response response = new EgovEISResource().searchEmployee(jsonData, loginResponse.getAccess_token());
-        Assert.assertEquals(response.getStatusCode(), 200);
-
         SearchEmployeeResponse searchEmployeeResponse = (SearchEmployeeResponse) ResponseHelper.getResponseAsObject(response.asString(), SearchEmployeeResponse.class);
+
+//        ###Assertions for various use cases###
         System.out.println("Number of Employees: "+searchEmployeeResponse.getEmployee().length);
+        Assert.assertEquals(response.getStatusCode(), 200);
         Assert.assertEquals(searchEmployeeResponse.getEmployee()[0].getUserName(), "egovernments");
         Assert.assertEquals(searchEmployeeResponse.getEmployee()[0].getCode(), "A9090");
-        Assert.assertEquals(searchEmployeeResponse.getEmployee()[0].getAssignments()[0].getPosition(), 1);
-        Assert.assertEquals(searchEmployeeResponse.getEmployee()[0].getAssignments()[0].getIsPrimary(), true);
+        Assert.assertEquals(searchEmployeeResponse.getEmployee()[0].getAssignments()[0].getPosition(), "4");
+        Assert.assertEquals(searchEmployeeResponse.getEmployee()[0].getAssignments()[0].getDesignation(), "2");
+        Assert.assertEquals(searchEmployeeResponse.getEmployee()[0].getAssignments()[0].getDepartment(), "1");
+        Assert.assertEquals(searchEmployeeResponse.getEmployee()[0].getAssignments()[0].getFromDate(), "2015-01-01");
+        Assert.assertEquals(searchEmployeeResponse.getEmployee()[0].getAssignments()[0].getToDate(), "2020-12-31");
+        Assert.assertEquals(searchEmployeeResponse.getEmployee()[0].getAssignments()[0].getIsPrimary(), "true");
+
+        Assert.assertEquals(searchEmployeeResponse.getEmployee()[1].getUserName(), "elzan");
+        Assert.assertEquals(searchEmployeeResponse.getEmployee()[1].getCode(), "1001");
+        Assert.assertEquals(searchEmployeeResponse.getEmployee()[1].getAssignments()[0].getPosition(), "1");
+        Assert.assertEquals(searchEmployeeResponse.getEmployee()[1].getAssignments()[0].getDesignation(), "2");
+        Assert.assertEquals(searchEmployeeResponse.getEmployee()[1].getAssignments()[0].getDepartment(), "1");
+        Assert.assertEquals(searchEmployeeResponse.getEmployee()[1].getAssignments()[0].getFromDate(), "2016-01-01");
+        Assert.assertEquals(searchEmployeeResponse.getEmployee()[1].getAssignments()[0].getToDate(), "2017-12-31");
+        Assert.assertEquals(searchEmployeeResponse.getEmployee()[1].getAssignments()[0].getIsPrimary(), "true");
     }
 }
