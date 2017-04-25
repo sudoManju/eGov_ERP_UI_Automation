e42d5e8c3580ded1ceb37b3c2a51c350f04686af
vinaykumarvvs <vinaykumarvvs1936@gmail.com>
2017-04-25 15:43:06 +0530

    [egsvc - 36 ] UI : Created the structure to fill the details of assignment in employee creation page

---
+++ egov/egov_functional_test/src/test/java/excelDataFiles/EmployeeManagementDetailsDataReader.java
@@ -0,0 +1,36 @@
+package excelDataFiles;
+
+import builders.employeeManagement.AssignmentDetailsBuilder;
+import entities.employeeManagement.AssignmentDetails;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+
+public class AssignmentDetailsDataReader extends ExcelReader {
+
+    Sheet assignmentDetailsSheet;
+
+    public AssignmentDetailsDataReader(String testData) {
+        super(testData);
+        assignmentDetailsSheet = workbook.getSheet("assignmentDetails");
+    }
+
+    public AssignmentDetails getAssignmentDetails(String dataName) {
+        Row dataRow = readDataRow(assignmentDetailsSheet, dataName);
+
+        String isPrimary = getCellData(assignmentDetailsSheet, dataRow, "IsPrimary").getStringCellValue();
+        String fromDate = convertNumericToString(assignmentDetailsSheet, dataRow, "FromDate");
+        String toDate = convertNumericToString(assignmentDetailsSheet, dataRow, "ToDate");
+        String mainDepartment = getCellData(assignmentDetailsSheet, dataRow, "MainDepartment").getStringCellValue();
+        String designation = getCellData(assignmentDetailsSheet, dataRow, "Designation").getStringCellValue();
+        String position = getCellData(assignmentDetailsSheet, dataRow, "Position").getStringCellValue();
+
+        return new AssignmentDetailsBuilder()
+                .withIsPrimary(isPrimary)
+                .withFromDate(fromDate)
+                .withToDate(toDate)
+                .withDepartment(mainDepartment)
+                .withDesignation(designation)
+                .withPosition(position)
+                .build();
+    }
+}
