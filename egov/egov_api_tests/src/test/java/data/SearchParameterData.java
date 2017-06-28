9b5d142d6da0fd8468cc85543fee85516f68ba55
karthik45 <karthikpenujuru@gmail.com>
2017-06-28 13:10:50 +0530

    [EGSVC-358]-api automation Modified the Data Parameter String

--- egov/egov_api_tests/src/test/java/data/SearchParameterData.java
+++ egov/egov_api_tests/src/test/java/data/SearchParameterData.java
@@ -5,5 +5,5 @@ public interface SearchParameterData {
     String IDS = "&ids=" ;
     String CODE = "&code=";
     String NAME = "&name=";
-    String NAMELOCAL = "&nameLocal";
+    String NAMELOCAL = "&nameLocal=";
 }
